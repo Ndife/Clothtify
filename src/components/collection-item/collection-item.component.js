c64aaef9516b273dfc0402a7c074bfed9f2e2375
NDIFE <stilesndife@gmail.com>
2020-08-17 13:49:27 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './collection-item.styles.scss';
+import React from "react";
+import { connect } from "react-redux";
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
+import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from "../custom-button/custom-button.component";
+
+import "./collection-item.styles.scss";
+
+const CollectionItem = ({ item, addItem }) => {
+    const { imageUrl, name, price } = item;
+  return (
     <div className="collection-item">
-        <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-);
+  );
+};
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
 
-
-
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,9 @@
-import { CartActionsTypes } from './cart.types'
+import { CartActionsTypes } from "./cart.types";
+import { addItemToCart } from "./cart.util";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,7 +11,12 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionsTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
-        hidden: !state.hidden ,
+        hidden: !state.hidden,
+      };
+    case CartActionsTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
