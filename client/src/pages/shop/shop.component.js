1f16cca385f515b2e447b04f3ab73119db606213
NDIFE <stilesndife@gmail.com>
2020-09-10 16:14:08 +0100

    adding optimization

--- client/src/pages/shop/shop.component.js
+++ client/src/pages/shop/shop.component.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 
-import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
-import CollectionPageContainer from "../collection/collection.container";
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import Spinner from "../../components/spinner/spinner.component";
+
+const CollectionsOverviewContainer = lazy(() => import("../../components/collections-overview/collections-overview.container"));
+const CollectionPageContainer = lazy(() => import("../collection/collection.container"));
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
@@ -13,7 +15,8 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
 
   return (
     <div className="shop">
-      <Route
+     <Suspense fallback={ <Spinner /> } >
+     <Route
         exact
         path={`${match.path}`}
         component={CollectionsOverviewContainer}
@@ -22,6 +25,7 @@ const ShopPage = ({ fetchCollectionsStart, match }) => {
         path={`${match.path}/:collectionId`}
         component={CollectionPageContainer}
       />
+     </Suspense>
     </div>
   );
 };
