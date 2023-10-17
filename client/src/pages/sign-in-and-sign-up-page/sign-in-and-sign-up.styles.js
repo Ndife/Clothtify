e94738ca43c6377c7c79d4af86c031f0f591a19d
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2023-10-17 07:47:27 +0100

    update signin/signup styles

--- client/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.styles.js
+++ client/src/pages/sign-in-and-sign-up-page/sign-in-and-sign-up.styles.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
 
 export const SignInAndSignUpContainer = styled.div`
-  width: 850px;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 30px auto;
   text-align: center;
 
-  @media screen and (max-width: 800px){
-    display: grid;
-    grid-template-columns: 1fr;
-  }
 `;
